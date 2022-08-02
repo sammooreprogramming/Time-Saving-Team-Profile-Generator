ad7dbc930da87e395479d388d2b72474dfa4a0f8
Sam Moore <sammooresemail23@gmail.com>
2022-08-02 14:24:41 -0700

    added arrays for respective positions

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
-const arrayTeam = [];
+const arrayOfTeam = [];
 
 // create an array of questions for the manager
 arrayManagerQuestions = [
