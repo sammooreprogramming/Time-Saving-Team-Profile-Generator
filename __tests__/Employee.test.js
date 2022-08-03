633c7a96c8ba05efb4d603f413a8a6c3b9197b08
Sam Moore <sammooresemail23@gmail.com>
2022-08-03 14:14:12 -0700

    major bug fixes and reformatting

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,41 +2,61 @@
 const Employee = require("../lib/Employee");
 
 // tests whether or not an object is created //
-test("Checks for the required information in Employee object", () => {
-    const employee = new Employee("Naruto Uzumaki", 1, "NarutoMail@konoha.com");
-    const object = "object";
-    expect(typeof(employee)).toBe(object);
+test("Checks for the creation of an Employee object", () => {
+    const employee = new Employee();
+    expect(typeof(employee)).toBe("object");
 })
 
 // name is tested for functionality correctness //
 test("Tests for the name of the employee", () => {
-    const name = "Naruto";
-    const employee = new Employee(name);
+    const employee = new Employee("Naruto Uzumaki");
+    const name = "Naruto Uzumaki";
 // using expect to get the user name input //
-    expect(employee.name).toBe(name);
+    expect(employee.name).toEqual(name);
 });
+
+// retrieves the name from the getName(). //
+    test("Tests to see if the name is retrieved by the program", () => {
+        const exampleName = "Sasuke Uchiha";
+        const employee = new Employee(exampleName);
+
+        expect(employee.getName()).toBe(exampleName);
+    })
+
 
 // ID is tested for functionality correctness //
 test("Tests for the ID of the employee", () => {
-    const id = 7;
-    const employee = new Employee("Naruto", id);
+    const employee = new Employee("Naruto Uzumaki", id);
+    const id = 1;
 // using expect to get the user ID input //
     expect(employee.id).toBe(id);
 });
 
+// retrieves the ID from the getID() method //
+test("Tests to see if the name is retrieved by the program", () => {
+    const exampleId = 1;
+    const employee = new Employee("Naruto Uzumaki", exampleId)
+})
+
 // email is tested for functionality correctness //
 test("Tests for the email of the employee", () => {
-    const email = "NarutoMail@konoha.com";
-    const employee = new Employee("Naruto", 2, "NarutoMail@konoha.com");
+    const employee = new Employee("Naruto", 7, "NarutoMail@konoha.com");
+    const email = "uzumakiMail@konoha.com"
 // using expect to get the user email input //
     expect(employee.email).toBe(email);
 });
+
+// retrieves the email from the getEmail() method //
+test("Tests to see if the email is retrieved by the program.", () => {
+    const exampleEmail = "NarutoMail@konoha.com";
+    const employee = new Employee("Naruto", exampleEmail)
+})
 
 // position is tested for functionality correctness //
 test("Tests for the position of the employee", () => {
     const position = "Employee";
     const employee = new Employee("Naruto Uzumaki", 1, "NarutoMail@konoha.com");
 // using expect to get the user position input //
-    expect(employee.getPosition()).toBe("Employee");
+    expect(employee.getPosition()).toBe(position);
 });
 
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,27 +1,27 @@
-// Engineer constructor required
+// Engineer constructor required. //
 const Engineer = require('../lib/Engineer');
 
-// Tests this to see if the program is able to create a github username
+// Tests this to see if the program is able to create a github username. //
 test("Tests whether a github is able to be created", () => {
     const githubExampleTest = "sammooreprogramming";
     const engineer = new Engineer("Sasuke Uchiha", 2, "sasukeuchiha@konohamail.com", githubExampleTest);
-// this test expects the github username to equal a string
-    expect(typeof(engineer)).toBe("object");
+// This test expects the github username to equal a string.
+    expect(typeof(engineer)).toEqual("object");
 });
 
-// this will retrieve the github username from the engineer
+// This will retrieve the github username from the engineer. //
 test("Tests whether the github username is retrieved.", () => {
     const githubExampleTest = "sammooreprogramming";
     const engineer = new Engineer("Sasuke Uchiha", 2, "sasukeuchiha@konohamail.com", githubExampleTest);
-// this test expects the 
-    expect(engineer.getGithub()).toBe(githubExampleTest);
+// This test expects the github username to be found by the program. //
+    expect(engineer.getGithub()).toEqual(githubExampleTest);
 });
 
-// this tests for the position of the engineer
+// This tests for the position of the engineer. //
 test("This tests for the position of the employee", () => {
     const confirmation = "Engineer";
     const githubExampleTest = "sammooreprogramming";
     const employee = new Engineer("Sasuke Uchiha", 2, "sasukeuchiha@konohamail.com", githubExampleTest);
 
-    expect(employee.getPosition()).toBe(confirmation);
+    expect(employee.getPosition()).toEqual(confirmation);
 });
